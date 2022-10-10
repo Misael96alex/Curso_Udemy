0f4c86350d2e104f05bb228e7a709b4226d0da4d
Misael Alexander Ajuchan Castillo <toony-a@hotmail.com>
2022-10-10 00:00:13 -0600

    Buscar Valores Vacios

--- Python Machin Learning/Spyder/carga_csv.py
+++ Python Machin Learning/Spyder/carga_csv.py
@@ -9,13 +9,24 @@ Created on Sun Oct  9 22:46:55 2022
 import pandas as pd
 # import os
 
+#url
 titaniccsv= "../datasets/titanic3.csv"
 titanicxls= "../datasets/titanic3.xls"
 titanicxlsx= "../datasets/titanic3.xlsx"
+titanicgit = "https://raw.githubusercontent.com/joanby/python-ml-course/master/datasets/titanic/titanic3.csv"
 
+#lecturas
 data1 = pd.read_csv(titaniccsv)
-
-data1.head()
 data2 = pd.read_excel(titanicxls, "titanic3")
-data2 = pd.read_excel(titanicxlsx, "titanic3")
+data3 = pd.read_excel(titanicxlsx, "titanic3")
+data4 = pd.read_csv(titanicgit)
+
+#encabezados
+data1.shape
+data2.shape
+data3.shape
+data4.shape
+
+# con F9 logras ver los outputs
+pd.isnull(data4["body"]).values.ravel().sum()
 
