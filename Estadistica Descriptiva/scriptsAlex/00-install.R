0ea009d3a07304c43f9bb6f8bd9aee5c441dae9f
Misael Alexander Ajuchan Castillo <toony-a@hotmail.com>
2022-10-14 01:00:29 -0600

    Clase 4

---
+++ Estadistica Descriptiva/scriptsAlex/00-install.R
@@ -0,0 +1,14 @@
+install.packages("tidyverse", dep=TRUE)
+
+library(tidyverse)
+
+install.packages("magic", dep=TRUE)
+
+library(magic)
+
+install.packages("abind", dep=TRUE)
+
+library(abind)
+
+magic(6)
+
---
+++ Estadistica Descriptiva/scriptsAlex/01-calculadora.R
@@ -0,0 +1,65 @@
+6/6
+7%/%3
+5+8*2/6
+2*2+2/2
+2%%3 #residuo
+
+pi*4
+
+pi
+2*Inf
+
+2^50
+
+1.1259e+15*10
+
+c(2^30, 1, 2, 3/2)
+
+c(2^50, 1, 2, 3/2)
+
+# raiz cuadrarda
+sqrt(9)
+
+#log
+
+log(3, base = 2)
+
+factorial(Inf)
+
+choose(20,5)
+
+factorial(10)
+
+# combinacion
+choose(5,3)
+
+# R asume que los angulos vienne en radianes
+
+sin(pi/2)
+cos(pi)
+sin(pi)
+
+cos(pi/3)
+
+sin (pi/3)
+
+# convertimos
+
+cos(60*pi/180)
+
+  3+2
+
+# arco coseno
+  
+  acos(pi)
+asin(0.8660254*pi/180)  
+
+
+# adelanto
+
+x=seq(0,2*pi,0.1)
+plot(x,sin(x),type = "l", col="blue", lwd=3, xlab=expression(x), ylab="")
+
+
+
+
